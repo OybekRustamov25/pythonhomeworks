4cabb617037dab03d8976bc0219e8a4f98734651
OybekRustamov25 <oybekrustamov867@gmail.com>
2025-02-12 21:07:45 +0500

    homework

---
+++ lesson-2/homework/number2.py
@@ -0,0 +1,7 @@
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+number3 = float(input("Enter third number: "))
+largest = max(number1, number2, number3)
+smallest = min(number1, number2, number3)
+print("Largest:", largest)
+print("Smallest:", smallest)
---
+++ lesson-2/homework/number3.py
@@ -0,0 +1,4 @@
+km = float(input("Enter distance in kilometers: "))
+meters = km * 1000
+centimeters = km * 100000
+print(f"{km} km is {meters} meters or {centimeters} centimeters.")
